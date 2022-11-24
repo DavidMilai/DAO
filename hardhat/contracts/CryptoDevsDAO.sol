44a2b49523aeebfb39f99d0b4c8c5ea33f33a37a
David Milai <davidmilai67@gmail.com>
2022-11-24 06:11:47 +0300

    extracted interfaces

--- hardhat/contracts/CryptoDevsDAO.sol
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -2,23 +2,8 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-interface IFakeNFTMarketplace {
-    function getPrice() external view returns (uint256);
-
-    function available(uint256 _tokenId) external view returns (bool);
-
-    function purchase(uint256 _tokenId) external payable;
-}
-
-interface ICryptoDevsNFT {
-    function balanceOf(address owner) external view returns (uint256);
-
-    function tokenOfOwnerByIndex(address owner, uint256 index)
-        external
-        view
-        returns (uint256);
-}
+import "./ICryptoDevsNFT.sol";
+import "./IFakeNFTMarketplace.sol";
 
 contract CryptoDevsDAO is Ownable {
     struct Proposal {
@@ -29,4 +14,11 @@ contract CryptoDevsDAO is Ownable {
         bool executed;
         mapping(uint256 => bool) voters;
     }
+
+    mapping(uint256 => Proposal) public proposals;
+
+    uint256 public numProposals;
+
+    IFakeNFTMarketplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
 }
