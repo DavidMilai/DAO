05afca30b6fadc2230b3f2f2105469cd852d793b
David Milai <davidmilai67@gmail.com>
2022-11-30 05:14:33 +0300

    deploying CryptoDevsDAO

--- hardhat/scripts/deployTwo.js
+++ hardhat/scripts/deployTwo.js
@@ -9,4 +9,24 @@ async function main() {
   await fakeNftMarketplace.deployed();
 
   console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+    {
+      // This assumes your account has at least 1 ETH in it's account
+      // Change this value as you want
+      value: ethers.utils.parseEther("1"),
+    }
+  );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
+}
+
+
+
+
+
 }
