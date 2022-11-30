5da3c5fe66430b7e02f14a69ebc078cd023b5772
David Milai <davidmilai67@gmail.com>
2022-11-30 05:13:32 +0300

    deploying fake nft market place

---
+++ hardhat/scripts/deployTwo.js
@@ -0,0 +1,12 @@
+const { ethers } = require("hardhat");
+const { CRYPTODEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+}
