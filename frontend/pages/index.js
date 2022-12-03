e966d3d3c4799fe71dc4991982bab30634c1a2c5
David Milai <davidmilai67@gmail.com>
2022-12-03 07:30:46 +0300

    variables

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,11 +12,27 @@ import {
 } from "../constants";
 import styles from "../styles/Home.module.css";
 
-
-
-
-
-
 export default function Home() {
+
+
+  // ETH Balance of the DAO contract
+  const [treasuryBalance, setTreasuryBalance] = useState("0");
+  // Number of proposals created in the DAO
+  const [numProposals, setNumProposals] = useState("0");
+  // Array of all proposals created in the DAO
+  const [proposals, setProposals] = useState([]);
+  // User's balance of CryptoDevs NFTs
+  const [nftBalance, setNftBalance] = useState(0);
+  // Fake NFT Token ID to purchase. Used when creating a proposal.
+  const [fakeNftTokenId, setFakeNftTokenId] = useState("");
+  // One of "Create Proposal" or "View Proposals"
+  const [selectedTab, setSelectedTab] = useState("");
+  // True if waiting for a transaction to be mined, false otherwise.
+  const [loading, setLoading] = useState(false);
+  // True if user has connected their wallet, false otherwise
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
+
+
 return()
 }
